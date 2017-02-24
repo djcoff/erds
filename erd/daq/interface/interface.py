03e52570fc64788dd959f83a2166310b202f448a
Derek <djcoff@gmail.com>
2017-02-24 15:51:48 +0000

    testing commit

--- erd/daq/interface/interface.py
+++ erd/daq/interface/interface.py
@@ -1,10 +1,19 @@
 from abc import ABCMeta, abstractmethod
+import asyncio
+"""
+Interface
+Abstract base class for HW Interface between Instrument class and "hardware". 
+Interface requires:
+   - event loop (asyncio) 
+   - configuration (dictionary with hw specific values)
+"""
 
 class Interface(metaclass=ABCMeta):
     
     def __init__(self):
         self.name = None
         self.type = None
+        self.loop = None
         
         #self.info = InterfaceInfo
         #self.config = InterfaceConfig
@@ -20,3 +29,34 @@ class Interface(metaclass=ABCMeta):
     @abstractmethod
     def configure(self): #second arg will be config info
         pass
+    
+    
+class SerialPort(Interface):
+    
+    def __init__(self):
+        super().__init__(self)
+        
+    def open(self):
+        pass
+    
+    def close(self):
+        pass
+    
+    def configure(self):
+        pass
+    
+    
+class TCPPort(Interface):
+    
+    def __init__(self):
+        super().__init__(self)
+        
+    def open(self):
+        pass
+    
+    def close(self):
+        pass
+    
+    def configure(self):
+        pass
+    
